b5bfbb46bc90350fb2917cba83b741af4d9abcf2
PriyandarshanB <131151638+PriyandarshanB@users.noreply.github.com>
2023-04-21 10:37:24 +0530

    Add files via upload

--- connect.php
+++ connect.php
@@ -5,10 +5,11 @@ if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
 $name = $_POST['username'];
-$Password = $_POST['password'];
+$email = $_POST['email'];
+$password = $_POST['password'];
 
-$sql = "INSERT INTO login (email,password)
-VALUES ('$name', '$Password')";
+$sql = "INSERT INTO login (username,email,password)
+VALUES ('$name', '$email','$password')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
--- loginform.php
+++ loginform.php
@@ -6,11 +6,14 @@
 <body>
 	<h2>Login</h2>
 	<form action="connect.php" method="post">
-		<label for="username">Username:</label>
+	<label for="username">Username:</label>
 		<input type="text" id="username" name="username"><br><br>
+		<label for="email">email:</label>
+		<input type="text" id="email" name="email"><br><br>
 		<label for="password">Password:</label>
 		<input type="password" id="password" name="password"><br><br>
-		<button type="submit" value="Login">submit</button>
+		
+		<button type="submit" value="insert">submit</button><br>
 	</form>
 </body>
 </html>
